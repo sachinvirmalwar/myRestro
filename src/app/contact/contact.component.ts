292c90b3310e6cb202b57e014ce2f8cd40e8a609
Sachin Virmalwar <sachin.virmalwar@gmail.com>
2019-07-14 21:04:48 +0530

    forms template 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
 import { Feedback, ContactType } from '../shared/feedback';
 
 
@@ -15,6 +14,8 @@ export class ContactComponent implements OnInit {
   feedback: Feedback;
   contactType = ContactType;
 
+  @ViewChild('fform') feedbackFormDirective;
+
   constructor( private fb: FormBuilder) {
     this.createform();
    }
@@ -25,15 +26,27 @@ export class ContactComponent implements OnInit {
   onSubmit(){
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset(
+      {
+        firstname: '',
+        lastname: '',
+        telnum: 0,
+        email: '',
+        agree: false,
+        contacttype: 'None',
+        message: ''
+      }
+    );
+    this.feedbackFormDirective.resetForm();
+    
   }
 
   createform(){
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['',Validators.required],
+      lastname: ['',Validators.required],
+      telnum: [0,Validators.required],
+      email: ['',Validators.required],
       agree: false,
       contacttype: 'None',
       message: ''
