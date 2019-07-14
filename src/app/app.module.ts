1fed121f0a18f0459133612738cbf91a142fe596
Sachin Virmalwar <sachin.virmalwar@gmail.com>
2019-07-14 19:22:36 +0530

    'forms'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -45,7 +49,11 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [
     PromotionService,
@@ -55,7 +63,7 @@ import { LoginComponent } from './login/login.component';
   entryComponents:[
     LoginComponent
   ],
-  
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
