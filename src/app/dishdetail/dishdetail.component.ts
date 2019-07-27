e2427e1073c7031dcd2c3f2d6ac65f622ddd9b3b
Sachin Virmalwar <sachin.virmalwar@gmail.com>
2019-07-27 21:29:45 +0530

    animations 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,11 +8,26 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { from } from 'rxjs';
 import { FormBuilder, FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'], 
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+          transform: 'scale(1.0)',
+          opacity: 1
+      })),
+      state('hidden', style({
+          transform: 'scale(0.5)',
+          opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+  ])
+]
 })
 
 
@@ -27,6 +42,7 @@ export class DishdetailComponent implements OnInit {
   commentForm: FormGroup;
   comment: Comment;
   dishcopy: Dish;
+  visibility = 'shown';
 
   formErrors = {
     'author': '',
@@ -57,12 +73,11 @@ export class DishdetailComponent implements OnInit {
 
     this.dishservice.getDishIds().subscribe((dishIds) => this.dishIds = dishIds);
 
-    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-    .subscribe(
-    dish =>{this.dish = dish;   
-      this.dishcopy = dish; 
-    this.setPrevNxt(dish.id); },
+    this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(params['id']); }))
+    .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNxt(dish.id); this.visibility = 'shown'; },
     errmess => this.errMess = <any>errmess);
+
+    
    // this.dish = this.dishservice.getDish(""+id);
     // this.dishservice.getDish(""+id).subscribe((dish) => this.dish = dish);
   }
